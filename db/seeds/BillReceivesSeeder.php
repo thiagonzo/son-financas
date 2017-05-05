485d26d0e6dab8d5753cd4cf17a55a40135a21fc
Thiago Rodrigues <Thiago Rodrigues>
2017-05-05 15:43:28 -0300

    criando estrutura de contas a receber

---
+++ db/seeds/BillReceivesSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class BillReceivesSeeder extends AbstractSeed
+{
+    const NAMES = [
+        'Salário',
+        'Bico',
+        'Restituição IR',
+        'Venda de produtos',
+        'Bolsa de valores',
+        'CDI',
+        'Tesouro Direto',
+        'Previdencia Privada'
+    ];
+
+    public function run()
+    {
+        $faker = \Faker\Factory::create('pt_BR');
+        $faker->addProvider($this);
+        $billReceives = $this->table('bill_receives');
+        $data=[];
+        foreach(range(1,20) as $value){
+            $data[] = [
+                'date_launch' => $faker->date(),
+                'name' => $faker->billReceivesName,
+                'value' => $faker->randomFloat(2,10,1000),
+                'user_id' => rand(1,4),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ];
+        }
+        $billReceives->insert($data)->save();
+    }
+
+    public function billReceivesName(){
+        return \Faker\Provider\Base::randomElement(self::NAMES);
+    }
+}
---
+++ src/Models/BillReceive.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Fin\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class BillReceive extends Model
+{
+	//Mass Assignment
+	protected $fillable = [
+		'date_launch',
+		'name',
+		'value',
+		'user_id'
+	];
+}
